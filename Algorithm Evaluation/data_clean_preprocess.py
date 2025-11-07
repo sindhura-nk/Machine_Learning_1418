b60c1aa3d2d795585ce672873c339f716550c821
sindhura-nk <nk.sindhura@gmail.com>
2025-11-07 11:47:52 +0530

    Algorithm Evaluation

---
+++ Algorithm Evaluation/data_clean_preprocess.py
@@ -0,0 +1,22 @@
+# To clean the data and preprocess it
+from sklearn.pipeline import make_pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler,OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+def clean_process_data(cat,con,X):
+    # Creating numerical pipeline that handles missing data and also scales numerical data
+    num_pipe = make_pipeline(SimpleImputer(strategy='mean'),StandardScaler())
+    # Creating categorical pipeline that handles missing data and scales text data
+    cat_pipe = make_pipeline(SimpleImputer(strategy='most_frequent'),OneHotEncoder(handle_unknown='ignore',sparse_output=False))
+    # combining both the pipelines
+    pre = ColumnTransformer([
+        ('cat',cat_pipe,cat),
+        ('con',num_pipe,con)
+    ]).set_output(transform='pandas')
+
+    # use the pre to fit and transform X data
+    X_pre = pre.fit_transform(X)
+
+    # return the preprocessed X 
+    return pre,X_pre
