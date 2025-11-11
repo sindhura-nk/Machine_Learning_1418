613be683caf419ee413b4ce31e61d5839a973449
sindhura-nk <nk.sindhura@gmail.com>
2025-11-11 12:02:11 +0530

    KMeans Clustering

--- Algorithm Evaluation/data_clean_preprocess.py
+++ Algorithm Evaluation/data_clean_preprocess.py
@@ -20,3 +20,25 @@ def clean_process_data(cat,con,X):
 
     # return the preprocessed X 
     return pre,X_pre
+
+# WRITE A CODE THAT HANDLES PREPROCESSING AND CLEANING OF NUMERICAL DATA ALONE.
+def clean_process_CON_data(X):
+    # Creating numerical pipeline that handles missing data and also scales numerical data
+    num_pipe = make_pipeline(SimpleImputer(strategy='mean'),StandardScaler()).set_output(transform='pandas')
+
+    # use the pre to fit and transform X data
+    X_pre = num_pipe.fit_transform(X)
+
+    # return the preprocessed X 
+    return num_pipe,X_pre
+
+# WRITE A CODE THAT HANDLES PREPROCESSING AND CLEANING OF categorical DATA ALONE.
+def clean_process_CAT_data(X):
+    # Creating numerical pipeline that handles missing data and also scales numerical data
+    cat_pipe = make_pipeline(SimpleImputer(strategy='most_frequent'),OneHotEncoder(handle_unknown='ignore',sparse_output=False)).set_output(transform='pandas')
+
+    # use the pre to fit and transform X data
+    X_pre = cat_pipe.fit_transform(X)
+
+    # return the preprocessed X 
+    return cat_pipe,X_pre
